d10d3da609f6cca5073bf8aab821c356c396638f
[TAREQUE-AHMED] <[tarequeahmed46@gmail.com]>
2024-09-18 18:43:18 +0600

    push done

---
+++ push.js
@@ -0,0 +1,12 @@
+const numbers = [33, 45, 67, 40, 90, 99];
+//console.log(numbers);
+
+const numbers2 = numbers.push(101);
+
+
+console.log(numbers);
+
+const ages = [32, 14, 15, 17, 28, 21];
+
+ages.push(102);
+console.log(ages);
